475d38e0c5bb70367b8c8fe01f65cc1c82bc10d6
DoomFungus <unspeakableferretofdoom@gmail.com>
2022-12-07 23:23:12 +0100

    Initial commit

---
+++ src/main/java/edu/bht/ase/redlib/RedLibApplication.java
@@ -0,0 +1,13 @@
+package edu.bht.ase.redlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedLibApplication.class, args);
+	}
+
+}
