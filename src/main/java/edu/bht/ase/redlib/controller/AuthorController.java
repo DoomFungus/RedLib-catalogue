ceec20d786142900455111ae7c6368770cec7e6c
DoomFungus <unspeakableferretofdoom@gmail.com>
2023-02-07 16:03:04 +0100

    fix(catalogue): fix controllers

--- src/main/java/edu/bht/ase/redlib/controller/AuthorController.java
+++ src/main/java/edu/bht/ase/redlib/controller/AuthorController.java
@@ -3,12 +3,10 @@ package edu.bht.ase.redlib.controller;
 import edu.bht.ase.redlib.dto.AuthorDto;
 import edu.bht.ase.redlib.service.interfaces.AuthorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/author")
+@RestController
+@RequestMapping("/author")
 @RequiredArgsConstructor
 public class AuthorController {
     private final AuthorService authorService;
--- src/main/java/edu/bht/ase/redlib/controller/BookController.java
+++ src/main/java/edu/bht/ase/redlib/controller/BookController.java
@@ -3,23 +3,21 @@ package edu.bht.ase.redlib.controller;
 import edu.bht.ase.redlib.dto.BookDto;
 import edu.bht.ase.redlib.service.interfaces.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/book")
+@RestController
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
 
     @GetMapping("/{id}")
-    public BookDto getBookById(@PathVariable(name = "id") Integer id){
+    public BookDto getBookById(@PathVariable(name = "id") Integer id) {
         return bookService.findBookById(id);
     }
 
     @PostMapping()
-    public BookDto createBook(BookDto bookDto){
+    public BookDto createBook(BookDto bookDto) {
         return bookService.createBook(bookDto);
     }
 }
